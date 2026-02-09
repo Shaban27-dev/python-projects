8d844f73d0728df921357a7755683d4183da518c
SHABAN-12 <alamsaban02@gmail.com>
2026-02-09 11:38:50 +0530

    Added Day 19 — File Organizer automation script using os and shutil to sort files into categorized folders

--- Mini_projects.py
+++ Mini_projects.py
@@ -1479,3 +1479,49 @@ while True:
         break
     else:
         print("Invalid choice. Please try again.")
+
+
+#          DAY-19
+# File Organizer Script
+# Sort files by extension
+# Create folders automatically
+import os
+import shutil
+
+folder_path = r"C:\Users\Lenovo\classroom\test_folder"
+
+file_map = {
+    ".txt": "Text",
+    ".jpg": "Images",
+    ".png": "Images",
+    ".mp3": "Music",
+    ".mp4": "Videos",
+    ".pdf": "PDF"
+}
+
+print("Scanning folder...\n")
+
+moved_count = 0  # Counter for moved files
+
+for file in os.listdir(folder_path):
+    source = os.path.join(folder_path, file)
+
+    # Skip folders
+    if not os.path.isfile(source):
+        continue
+
+    file_ext = os.path.splitext(file)[1].lower()
+
+    if file_ext in file_map:
+        target_folder = os.path.join(folder_path, file_map[file_ext])
+
+        if not os.path.exists(target_folder):
+            os.makedirs(target_folder)
+
+        destination = os.path.join(target_folder, file)
+        shutil.move(source, destination)
+
+        print(f"Moving {file} → {file_map[file_ext]}")
+        moved_count += 1  # Increment counter
+
+print(f"\nOrganizing complete! Moved {moved_count} files successfully.")
--- README.md
+++ README.md
@@ -406,6 +406,39 @@ Player vs computer
 First to 5 wins becomes champion 🏆
 
 
+🗂️⚙️ Day 19 — File Organizer Script (Automation)
+
+A Python automation script that scans a folder and automatically organizes files into categorized subfolders based on their file extensions 📁✨.
+
+🧾 Features
+
+🔍 Scans the selected folder for files
+
+📂 Automatically creates category folders (Text, Images, Music, Videos, PDF)
+
+📥 Moves files into their respective folders
+
+🚫 Skips existing folders safely
+
+📊 Displays how many files were organized
+
+🧠 Concepts Used
+
+Concepts used: 🗂️ file system automation, 📁 os module, 🚚 shutil module, 🔁 loops, 🔍 file extension handling, ⚙️ scripting for productivity.
+
+📌 Supported File Categories
+
+.txt → Text
+
+.jpg, .png → Images
+
+.mp3 → Music
+
+.mp4 → Videos
+
+.pdf → PDF
+
+
 👨‍💻 Author
 
 SHABAN ALAM | 
